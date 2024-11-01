bbfd5ff02109e905535b4ee58ea078afb29e2576
Leonardo <leo70x7000@gmail.com>
2024-11-01 09:59:08 -0300

    Adiciona relacionamento de Livro com Categoria e Editora

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,5 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 
+from .categoria import Categoria
+from .editora import Editora
+
 from core.models import Livro
 from core.serializers import LivroSerializer
 
@@ -7,3 +10,9 @@ from core.serializers import LivroSerializer
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+    
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
